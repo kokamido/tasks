a7f68ebc4d91a9bff00842f177a217ab7d9c6aef
Albert <khoramus@yandex.ru>
2017-04-09 12:06:01 +0500

    After 1 release. Хочу понять что требуется, так как программана на первом релизе крашилась, и не давала понять

---
+++ SimQL/SimQl_Tests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace SimQLTask
+{
+    [TestFixture]
+    class SimQl_Tests
+    {
+        [Test]
+        public void TestInTask()
+        {
+            //arrenge
+            var input =@"{
+    'data': {
+        'a': {
+            'x':3.14, 
+            'b': {'c':15}, 
+            'c': {'c':9}
+        }, 
+        'z':42
+    },
+    'queries': [
+        'a.b.c',
+        'z',
+        'a.x'
+    ]
+}";
+            var expected = @"a.b.c = 15
+z = 42
+a.x = 3.14";
+            //act
+            var resultFull = "";
+            foreach (var result in SimQLProgram.ExecuteQueries(input))
+                resultFull += $"{result}\r\n";
+
+            //Assert
+            Assert.AreEqual(expected, resultFull.Substring(0, resultFull.Length-2));
+
+        }
+        [Test]
+        public void TestInRelease1()
+        {
+            //arrenge
+            var input = @"{""data"":{""empty"":{},""ab"":0,""x1"":1,""x2"":2,""y1"":{""y2"":{""y3"":3}}},""queries"":[""empty"",""xyz"",""x1.x2"",""y1.y2.z"",""empty.foobar""]}";
+            var expected = @"empty =
+xyz = 
+x1.x2 = 
+y1.y2.z = 
+empty.foobar = 
+";
+            //act
+            var resultFull = "";
+            foreach (var result in SimQLProgram.ExecuteQueries(input))
+                resultFull += $"{result}\r\n";
+
+            //Assert
+            Assert.AreEqual(expected, resultFull.Substring(0, resultFull.Length - 2));
+
+        }
+    }
+}
