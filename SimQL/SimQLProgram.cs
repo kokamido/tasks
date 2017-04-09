a40bd80be3809e1e805c5cd06414f032c79540f3
Albert <khoramus@yandex.ru>
2017-04-09 11:07:28 +0500

    Init SimQL

--- SimQL/SimQLProgram.cs
+++ SimQL/SimQLProgram.cs
@@ -11,10 +11,34 @@ namespace SimQLTask
 	{
 		static void Main(string[] args)
 		{
-			var json = Console.In.ReadToEnd();
-			foreach (var result in ExecuteQueries(json))
-				Console.WriteLine(result);
-		}
+            /*TestMain();*/
+            var json = Console.In.ReadToEnd();
+            foreach (var result in ExecuteQueries(json))
+                Console.WriteLine(result);
+
+        }
+
+        static void TestMain()
+        {
+            var json = @"{
+    'data': {
+        'a': {
+            'x':3.14, 
+            'b': {'c':15}, 
+            'c': {'c':9}
+        }, 
+        'z':42
+    },
+    'queries': [
+        'a.b.c',
+        'z',
+        'a.x'
+    ]
+}";
+            foreach (var result in ExecuteQueries(json))
+                Console.WriteLine(result);
+            Console.ReadKey();
+        }
 
 		public static IEnumerable<string> ExecuteQueries(string json)
 		{
@@ -22,7 +46,34 @@ namespace SimQLTask
 			var data = (JObject)jObject["data"];
 			var queries = jObject["queries"].ToObject<string[]>();
 			// TODO
-			return queries.Select(q => "TODO");
+			return queries.Select(q => $"{q} = {WEfyhjj(q, data)}");
 		}
+
+        public static string WEfyhjj(string path, JObject data)
+        {
+            data = JObject.Parse(JsonConvert.SerializeObject(data));
+            
+            var splittedPath = path.Split(new[] { '.' }, StringSplitOptions.RemoveEmptyEntries);
+            JToken jToken;
+            double value = 0;
+            foreach (var property in splittedPath)
+            {
+
+                bool isTryingSuccessful = data.TryGetValue(property, out jToken);
+                if (!isTryingSuccessful)
+                {
+                    throw new Exception("No field");
+                }
+                try
+                {
+                    data = (JObject)jToken;
+                }
+                catch (Exception ex)
+                {
+                    value = (double)jToken;
+                }
+            }
+            return value.ToString(System.Globalization.CultureInfo.InvariantCulture);
+        }
 	}
 }
